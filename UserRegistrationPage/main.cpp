247e737dbad4cbef823d4f3a57baeab9c2783eff
Gizem EROL <gizerol@hotmail.com>
2025-03-28 12:49:41 +0300

    Updated frames and edit borders.

--- UserRegistrationPage/main.cpp
+++ UserRegistrationPage/main.cpp
@@ -19,22 +19,25 @@ void ICGUI_main()
 {
     ICGUI_Create();
 
-    ICG_Static(50, 20, 80, 25, "Ad:");
-    NameInput = ICG_SLEditSunken(150, 20, 200, 25, "");
+    ICG_MLStatic(50, 20, 80, 25, "Ad:");
+    NameInput = ICG_SLEditBorder(150, 20, 200, 25, "");
 
-    ICG_Static(50, 60, 80, 25, "Soyad:");
-    SurnameInput = ICG_SLEditSunken(150, 60, 200, 25, "");
+    ICG_MLStatic(50, 60, 80, 25, "Soyad:");
+    SurnameInput = ICG_SLEditBorder(150, 60, 200, 25, "");
 
-    ICG_Static(50, 100, 80, 25, "Telefon:");
-    PhoneInput = ICG_SLEditSunken(150, 100, 200, 25, "");
+    ICG_MLStatic(50, 100, 80, 25, "Telefon:");
+    PhoneInput = ICG_SLEditBorder(150, 100, 200, 25, "");
 
-    ICG_Static(50, 140, 80, 25, "E-posta:");
-    EmailInput = ICG_SLEditSunken(150, 140, 200, 25, "");
+    ICG_MLStatic(50, 140, 80, 25, "E-posta:");
+    EmailInput = ICG_SLEditBorder(150, 140, 200, 25, "");
 
-    RegisterButton = ICG_Button(150, 180, 100, 30, "Kayýt Ekle", RegisterCustomer);
-    UploadPhotoButton = ICG_Button(400, 180, 100, 30, "Fotoðraf Yükle", UploadPhoto);
+    ICG_MLStatic(50, 180, 80, 25, "Þifre:");
+    EmailInput = ICG_SLPasswordB(150, 180, 200, 25);
 
-    PhotoFrame = ICG_FramePanel(380, 20, 150, 150); // Fotoðraf önizleme alaný
+    RegisterButton = ICG_Button(150, 220, 100, 30, "Kayýt Ekle", RegisterCustomer);
+    UploadPhotoButton = ICG_Button(400, 180, 100, 40, "Fotoðraf Yükle", UploadPhoto);
+
+    PhotoFrame = ICG_FrameThick(380, 20, 150, 150); // Fotoðraf önizleme alaný
 }
 
 
